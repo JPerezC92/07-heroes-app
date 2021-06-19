b6f7593d549d2b822c81842cc52217289036294c
Philip Perez Castro <jperez.c92@gmail.com>
2021-06-18 23:26:49 -0500

    lesson 177

--- src/components/heroes/HeroScreen.tsx
+++ src/components/heroes/HeroScreen.tsx
@@ -1,11 +1,51 @@
-import React from "react";
+import { Redirect, useHistory, useParams } from "react-router-dom";
+import { getHeroById } from "../../selectors/getHeroesByPublisher";
 
-interface Props {}
+const HeroScreen = () => {
+  const { heroeId = "" }: { heroeId: string } = useParams();
+  const history = useHistory();
 
-const HeroScreen = (props: Props) => {
+  console.log(history);
+  const hero = getHeroById(heroeId);
+
+  console.log(history.length);
+
+  const handleClick = () =>
+    history.length > 2 ? history.goBack() : history.push("/");
+
+  if (!hero) return <Redirect to="/" />;
+
   return (
-    <div>
-      <h1>Heroe Screen</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          className="img-thumbnail"
+          src={`../assets/heroes/${heroeId}.jpg`}
+          alt={hero.superhero}
+        />
+      </div>
+
+      <div className="col-8">
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego:</b> {hero.alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher:</b> {hero.publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First Appearance:</b> {hero.first_appearance}
+          </li>
+        </ul>
+
+        <h5>Characters</h5>
+
+        <p>{hero.characters}</p>
+
+        <button className="btn btn-outline-info" onClick={handleClick}>
+          Return
+        </button>
+      </div>
     </div>
   );
 };
