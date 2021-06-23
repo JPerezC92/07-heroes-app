14ccfa3dc262871e1e9380d780a302fc29dd93a8
Philip Perez Castro <jperez.c92@gmail.com>
2021-06-23 13:21:03 -0500

    FIX on PrivateRoute test

--- src/routers/PrivateRoute.test.tsx
+++ src/routers/PrivateRoute.test.tsx
@@ -2,13 +2,6 @@ import PrivateRoute from "./PrivateRoute";
 import { MemoryRouter } from "react-router-dom";
 import { mount } from "enzyme";
 import LocalStorageService from "../services/LocalStorageService";
-import { useAuthState } from "../auth/AuthContext";
-
-jest.mock("../auth/AuthContext", () => {
-  return {
-    useAuthState: jest.fn(),
-  };
-});
 
 describe("Test on <PrivateRoute />", () => {
   const props: unknown = { location: { pathname: "/marvel" } };
@@ -16,10 +9,6 @@ describe("Test on <PrivateRoute />", () => {
   LocalStorageService.save = jest.fn();
 
   test("Should render if user is authenticated and save url on localStorage", () => {
-    (useAuthState as jest.MockedFunction<any>).mockReturnValue({
-      user: { logged: true, name: "Philip" },
-    });
-
     const wrapper = mount(
       <MemoryRouter>
         <PrivateRoute
@@ -42,10 +31,6 @@ describe("Test on <PrivateRoute />", () => {
   });
 
   test("shouldn't render component if user isn't authenticated", () => {
-    (useAuthState as jest.MockedFunction<any>).mockReturnValue({
-      user: { logged: false, name: null },
-    });
-
     const wrapper = mount(
       <MemoryRouter>
         <PrivateRoute
@@ -53,7 +38,7 @@ describe("Test on <PrivateRoute />", () => {
           isAuthenticated={false}
           path="/"
           Component={() => {
-            return <span>Hola</span>;
+            return <span>hi</span>;
           }}
         />
       </MemoryRouter>
