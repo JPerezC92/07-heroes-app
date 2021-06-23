a93d3236fc6e41d57ac764be1919a70bef50e663
Philip Perez Castro <jperez.c92@gmail.com>
2021-06-22 21:47:52 -0500

    Configured Enzyme

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,15 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import Enzyme from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { createSerializer } from "enzyme-to-json";
+
+type SnapshotSerializerPlugin = import("pretty-format").Plugin;
+
+Enzyme.configure({ adapter: new Adapter() });
+
+expect.addSnapshotSerializer(
+  createSerializer({ mode: "deep" }) as unknown as SnapshotSerializerPlugin
+);
